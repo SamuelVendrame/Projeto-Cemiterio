f4e489a4673c7febe541f101f25603851926d9d8
SamuelVendrame <samuelvbomfim@gmail.com>
2026-01-29 16:38:33 -0300

    Adicionado o modal de registro, adaptando-o a com formulário

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -10,3 +10,30 @@ const buscarPrimeiroUser = (req, res) => {
 router.get("/buscarDadosDoPrimeiroUsuario", buscarPrimeiroUser)
 
 module.exports = router;
+
+/*
+
+app.get("/produtos/", (req, res) => {
+        res.json(produtos)
+    })
+
+app.get("/produtos/:id", (req, res) => {
+    const produtoId = Number(req.params.id)
+
+    const produto = produtos.find(produtoId => produtoId.produtos === produtoId)
+
+    res.json(produto)
+    })
+
+app.post("/produtos/:id/desconto", (req, res) =>{
+        const produtoId = Number(req.params.id)
+
+        const produto = produtos.find(produtoId => produtoId.produtos === produtoId)
+
+        produto.desconto = 0.10
+
+        res.json(produto)
+
+    })
+
+*/
