ad5b5ebea68245e5b473e3ee879b72a42bfaa077
SamuelVendrame <samuelvbomfim@gmail.com>
2026-02-08 22:03:03 -0300

    Adicionando pequenas mudancas visuais de margin-bottom e padding na lista displayada de pesquisa na rota admin

--- backend/data/data.js
+++ backend/data/data.js
@@ -3,38 +3,43 @@ const registros = [
     nome: "Ana",
     dataNascimento: "1958-03-22",
     dataFalencimento: "2023-05-14",
-    outraPessoaNome: "Carlos"
+    outraPessoaNome: "Carlos",
   },
   {
     nome: "Pedro",
     dataNascimento: "1967-11-09",
     dataFalencimento: "2023-06-01",
-    outraPessoaNome: "Maria"
+    outraPessoaNome: "Maria",
   },
   {
     nome: "Gabriel",
     dataNascimento: "1975-01-15",
     dataFalencimento: "2023-06-18",
-    outraPessoaNome: "Lucas"
+    outraPessoaNome: "Lucas",
   },
   {
     nome: "João",
     dataNascimento: "1962-08-30",
     dataFalencimento: "2023-07-02",
-    outraPessoaNome: "Fernanda"
+    outraPessoaNome: "Fernanda",
   },
   {
     nome: "Ytalo",
     dataNascimento: "1980-04-12",
     dataFalencimento: "2023-07-20",
-    outraPessoaNome: "Rafael"
+    outraPessoaNome: "Rafael",
   },
   {
     nome: "Samuel",
     dataNascimento: "1971-12-03",
     dataFalencimento: "2023-08-05",
-    outraPessoaNome: "Daniel"
-  }
+    outraPessoaNome: "Daniel",
+  },
+  {
+    nome: "Sucinto Pedro",
+    dataNascimento: "1971-12-03",
+    dataFalencimento: "2023-08-05",
+  },
 ];
 
 module.exports = registros;
